f31e68aaa3c894399fcfed8a899946c38f7d9765
Krunal Patel <krunalpatell>
2022-01-31 21:03:26 -0500

    hello

---
+++ Homework/src/Lab.java
@@ -0,0 +1,13 @@
+public class Lab {
+    public static void main (String[]args) {
+        int x = 43560;
+        int y = 389767;
+        //where the x is oneAcre, y is total value and z is result
+        int z = y / x;
+        System.out.println(z + " acre ");
+
+
+    }
+}
+
+
---
+++ Homework/src/NameAgeAnnualIncome.java
@@ -0,0 +1,20 @@
+public class NameAgeAnnualIncome {
+    public static void main(String[] args){
+
+        String name;      //To hold a name
+        int age;          //To hold an age
+        double annualPay; //To hold annualPay
+
+        //Store value in the string object and variables.
+        name = "Runald";
+        age = 19;
+        annualPay = 30000.0;
+
+        //Display a message.
+
+        System.out.println("name = " + " Runald");
+        System.out.println("age = " + 20);
+        System.out.println("annualPay = " + 30000);
+    }
+}
+
